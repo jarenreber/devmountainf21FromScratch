9b3eef54dfe704de678b70234ee8d9b72295c06f
jarenreber <jaren.c.reber@gmail.com>
2022-04-28 12:33:27 -0600

    hello world

--- server/index.js
+++ server/index.js
@@ -3,6 +3,17 @@ const path = require('path')
 
 const app = express()
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '77dc81e6de3c4500b14f044d4dec9817',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/index.html'))
 })
